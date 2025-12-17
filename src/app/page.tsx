76aefae88509d17ab8d19577d1c5194293a303f2
Ali Hasnain <hafizalihasnain465@gmail.com>
2025-12-17 16:42:06 +0500

    VIBE-5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,22 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/hasnain.jpeg" alt="" width="384" height="512"/>
-  <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <div><figure className="md:fbg-green-800 rounded-x5 p-8 green:bg-green-800">
+  <img className="w-40 h-50 rounded-full mx-auto" src="/hasnain.jpeg" alt="" width="384" height="512"/>
+  <div className="pt-6 text-center space-y-8"> 
     <blockquote>
-      <p className="text-lg font-medium">
+      <p className="text-center font-medium">
         “Tailwind CSS is the only framework that I've seen scale
         on large teams. It’s easy to customize, adapts to any design,
         and the build size is tiny.”
       </p>
     </blockquote>
     <figcaption className="font-medium">
-      <div className="text-sky-500 dark:text-sky-400">
+      <div className="text-red-500 dark:text-blue-400">
         PAIB
       </div>
-      <div className="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Ali Hasnain Gujjar
+      <div className="text-green-700 dark:text blue-500">
+        Staff Engineer,Ali Hasnain
       </div>
     </figcaption>
   </div>
